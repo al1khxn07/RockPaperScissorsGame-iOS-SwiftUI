1877ebdc09d7f74601f4b7e843c05dd1b63fd2e3
Alikhan Zhanibek <bayalikhan@bk.ru>
2025-10-30 17:51:16 -0500

    Initial Commit

---
+++ RPSGame/RPSGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSGameApp.swift
+//  RPSGame
+//
+//  Created by Alikhan Zhanibek on 2025-10-30.
+//
+
+import SwiftUI
+
+@main
+struct RPSGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
